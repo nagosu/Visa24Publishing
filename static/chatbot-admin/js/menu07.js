3edcfb2f2cfd2c36b57ad93aaf0a0fd55932bf2b
nagosu <nozick1021@gmail.com>
2024-05-19 17:00:35 +0900

    대행 신청 업체 퍼블리싱

---
+++ static/chatbot-admin/js/menu07.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleList = document.querySelectorAll(".agency__toggle-switch");
+
+  // 토글 ON/OFF 텍스트 색상 변경
+  toggleList.forEach(($toggle) => {
+    $toggle.addEventListener("click", () => {
+      $toggle.classList.toggle("active");
+
+      const isActive = $toggle.classList.contains("active");
+      const statusOff = $toggle.parentElement.querySelector(
+        ".agency__toggle-visa"
+      );
+      const statusOn = $toggle.parentElement.querySelector(
+        ".agency__toggle-company"
+      );
+
+      if (isActive) {
+        statusOff.style.color = "#d4d4d4";
+        statusOn.style.color = "#1b3133";
+      } else {
+        statusOff.style.color = "#1b3133";
+        statusOn.style.color = "#d4d4d4";
+      }
+    });
+  });
+});
