4ab461d93b0489e30e550b886a3df3ea4369a972
nagosu <nozick1021@gmail.com>
2024-05-22 02:37:27 +0900

    admin 퍼블리싱 완료

--- static/chatbot-admin/js/menu02.js
+++ static/chatbot-admin/js/menu02.js
@@ -2,17 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const searchButton = document.querySelector(
     ".content-modify__sidebar-search-button"
   );
-  const modalAll = document.querySelectorAll(".modal-background");
-  const modalFind = document.querySelector(".modal-background.find");
 
   // 조회 버튼 클릭 시 모달 창 열기
+  const modalFind = document.querySelector(".modal-background.find");
   searchButton.addEventListener("click", function () {
     modalFind.style.display = "flex";
   });
 
   // 확인 버튼 클릭 시 모달 창 닫기
   const modalConfirmButton = document.getElementById("modalConfirmButton");
-
   modalConfirmButton.addEventListener("click", function () {
     modalFind.style.display = "none";
   });
@@ -20,7 +18,6 @@ document.addEventListener("DOMContentLoaded", function () {
   // 저장 버튼 클릭 시 모달 창 열기
   const saveButton = document.querySelector(".content-modify__save-button");
   const modalSave = document.querySelector(".modal-background.save");
-
   saveButton.addEventListener("click", function () {
     modalSave.style.display = "flex";
   });
@@ -28,7 +25,6 @@ document.addEventListener("DOMContentLoaded", function () {
   // 취소/확인 버튼 클릭 시 모달 창 닫기
   const modalCancelButton = document.getElementById("modalCancelButton");
   const modalSaveButton = document.getElementById("modalSaveButton");
-
   modalCancelButton.addEventListener("click", function () {
     modalSave.style.display = "none";
   });
@@ -37,11 +33,10 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // 삭제 버튼 클릭 시 모달 창 열기
-  const deleteButtons = document.querySelectorAll(
-    ".usage-history__table-item--delete button"
+  const deleteIcon = document.querySelectorAll(
+    ".usage-history__table-item--delete img"
   );
-
-  deleteButtons.forEach((button) => {
+  deleteIcon.forEach((button) => {
     button.addEventListener("click", function () {
       const modalDelete = document.querySelector(".modal-background.delete");
       modalDelete.style.display = "flex";
@@ -52,12 +47,45 @@ document.addEventListener("DOMContentLoaded", function () {
   const modalDeleteCancelButton = document.querySelector(".delete-cancel");
   const modalDeleteSaveButton = document.querySelector(".delete-save");
   const modalDelete = document.querySelector(".modal-background.delete");
-
   modalDeleteCancelButton.addEventListener("click", function () {
     modalDelete.style.display = "none";
   });
-
   modalDeleteSaveButton.addEventListener("click", function () {
     modalDelete.style.display = "none";
   });
+
+  // 업무 비용 수정
+  function addPencilClickListener(className) {
+    const pencilIcon = document.querySelector(`.${className}`);
+    pencilIcon.addEventListener("click", function () {
+      const span = pencilIcon.previousElementSibling;
+      const value = span.innerText;
+      const input = document.createElement("input");
+      input.type = "text";
+      input.className = "content-modify__charge-text money";
+      input.value = value;
+
+      span.replaceWith(input);
+
+      input.focus();
+      input.setSelectionRange(value.length, value.length);
+
+      // Enter key 이벤트로 다시 span으로 변경
+      input.addEventListener("blur", function () {
+        const newSpan = document.createElement("span");
+        newSpan.className = "content-modify__charge-text money";
+        newSpan.innerText = input.value;
+        input.replaceWith(newSpan);
+      });
+
+      input.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+          input.blur();
+        }
+      });
+    });
+  }
+
+  addPencilClickListener("pencil-docs");
+  addPencilClickListener("pencil-agency");
 });
--- static/chatbot-admin/js/menu04.js
+++ static/chatbot-admin/js/menu04.js
@@ -65,7 +65,7 @@ document.addEventListener("DOMContentLoaded", function () {
             <img src="../../static/chatbot-admin/images/Edit_fill.svg" />
           </th>
           <th class="usage-history__table-item usage-history__table-item--no delete-icon">
-            <img src="../../static/chatbot-admin/images/Trash.svg" />
+            <img src="../../static/chatbot-admin/images/Trash.svg" class="agency-delete"/>
           </th>
         </tr>
       `;
@@ -112,5 +112,47 @@ document.addEventListener("DOMContentLoaded", function () {
         modal.style.display = "none";
       });
     });
+  });
+
+  const modalBackground = document.querySelector(".modal-background.delete");
+  const modalFirst = document.querySelector(".modal.first");
+  const modalSecond = document.querySelector(".modal.second");
+  const modalSaveButton = document.getElementById("modalSaveButton");
+  const modalCancelButton = document.getElementById("modalCancelButton");
+  const modalConfirmButton = document.getElementById("modalConfirmButton");
+
+  // 이벤트 위임을 사용하여 agencyDelete 클래스의 클릭 이벤트 처리
+  document.body.addEventListener("click", function (event) {
+    if (event.target.classList.contains("agency-delete")) {
+      modalBackground.style.display = "flex";
+      modalFirst.style.display = "flex";
+    }
+  });
+
+  // 저장 버튼 클릭 시 첫 번째 모달 닫고 두 번째 모달 열기
+  modalSaveButton.addEventListener("click", function () {
+    modalFirst.style.display = "none";
+    modalSecond.style.display = "flex";
+  });
+
+  // 확인 버튼 클릭 시 두 번째 모달 창 닫기
+  modalConfirmButton.addEventListener("click", function () {
+    modalBackground.style.display = "none";
+    modalSecond.style.display = "none";
+  });
+
+  // 아니오 버튼 클릭 시 첫 번째 모달 창 닫기
+  modalCancelButton.addEventListener("click", function () {
+    modalBackground.style.display = "none";
+    modalFirst.style.display = "none";
+  });
+
+  // 모달 배경 클릭 시 모달 창 닫기
+  modalBackground.addEventListener("click", function (event) {
+    if (event.target === modalBackground) {
+      modalBackground.style.display = "none";
+      modalFirst.style.display = "none";
+      modalSecond.style.display = "none";
+    }
   });
 });
